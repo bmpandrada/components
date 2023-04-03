ed0c89c9e65b215885e7e4726a5db86695a75299
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-04-03 18:36:36 +0800

    adding hero pages

--- src/component/carousel/data.jsx
+++ src/component/carousel/data.jsx
@@ -7,9 +7,9 @@ import img4 from './img/premium_photo-1673835488393-f54cf144d507.avif'
  
 export const carousel = [
     {id:1, job:'web dev', author:'bruce',  img_lik:img1, text:`In the 1960s with the release of Letraset sheets Lorem Ipsum  passages,` },
-    {id:2, job:'engineer', author:'bea', img_lik:img2, text:`Remaining essent release of Letraset sheets containing Lorem Ipsum passages,` },
+    {id:2, job:'engineer', author:'bea', img_lik:img2, text:`Remaining essent release of Letraset sheets containing Lorem Ipsum,` },
     {id:3, job:'web dev', author:'phil', img_lik:img3, text:`The printing and  of Letraset sheets contai containing Lorem Ipsum,` },
-    {id:4, job:'web dev', author:'saki', img_lik:img4, text:`Typesettings passages  Lorem Ipsum passages containing, Letraset sheets contai ` },
+    {id:4, job:'web dev', author:'saki', img_lik:img4, text:`Typesettings passages  Lorem Ipsum passages containing, Letraset sheets ` },
 ]
 
 
