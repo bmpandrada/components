2ae180ba0d5e065c8941b9fd743d21b97a36a1fc
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-04-03 11:12:11 +0800

    my component layouts

--- src/component/pages/Pages.jsx
+++ src/component/pages/Pages.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import './pages.css'
 
 const Pages = () => {
   return (
     <div className='paganation'>
-        <ul className="pg-container">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>...</li>
-        </ul>
-        <button>prev</button>
-        <button>next</button>
+      
+        <button className='btn-page'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="btn-page-icon w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
+</svg>
+</button>
+        
+<a href="" className='page-link'>1</a>
+<a href="" className='page-link'>2</a>
+<a href="" className='page-link'>3</a>
+<a href="" className='page-link'>4</a>
+<a href="" className='page-link'>5</a>
+<a href="" className='page-link'>6</a>
+<span className='the-dots '>...</span>
+<a href="" className='page-link'>23</a>
+<button className='btn-page'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="btn-page-icon w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+</svg>
+</button>
     </div>
   )
 }
